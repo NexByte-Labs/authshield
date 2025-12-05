fc27c630bcb02d664e21e87501ce66f104bd6408
Mohammed Imraz <130502530+mohammedimraz0@users.noreply.github.com>
2025-12-05 15:37:42 +0530

    Create user.model.js

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  name: { type: String },
+  role: { type: String, enum: ['admin', 'manager', 'user'], default: 'user' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
